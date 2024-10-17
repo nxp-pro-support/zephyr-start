5103d8fc4272e3b27b695d74ebdb6a8942a1a868
Eli Hughes <eli.hughes@nxp.com>
2024-10-17 16:41:25 -0400

    fixed I2C demo so it could build for things other than FRDM-MCXN947

--- demos/i2c_shell/src/main.c
+++ demos/i2c_shell/src/main.c
@@ -10,6 +10,8 @@
 #include <zephyr/shell/shell.h>
 #include <zephyr/logging/log.h>
 
+#if CONFIG_BOARD_FRDM_MCXN947 == 1
+
 #define TEMP_SENSOR_I2C_ADDRESS		0X48
 #define TEMP_SENSOR_RESOLUTION		0.0625f		//0.0625 °C
 
@@ -17,6 +19,8 @@ static const struct device * i2c5_bus = DEVICE_DT_GET(DT_NODELABEL(flexcomm5_lpi
 
 int extra_clock_init()
 {
+
+
     CLOCK_SetClkDiv(kCLOCK_DivFlexcom5Clk, 1u); 
     CLOCK_AttachClk(kFRO12M_to_FLEXCOMM5);
 
@@ -48,24 +52,28 @@ static int read_temperature_handler(const struct shell *shell, size_t argc, char
 	temperature_register_value = ((int16_t)read_values[0] << 8) | (int16_t)read_values[1];
 	temperature_value = (temperature_register_value >> 4) * TEMP_SENSOR_RESOLUTION;
 	
-	shell_fprintf(shell, SHELL_VT100_COLOR_DEFAULT, "Temperature value %.3f \n", temperature_value);
+	shell_fprintf(shell, SHELL_VT100_COLOR_DEFAULT, "Temperature value %.1f \n", (double)temperature_value);
 
 	return 0; 
 }
 
 SHELL_CMD_REGISTER(read_temperature, NULL, "Read the P3T1755DP temperature sensor of the FRDM board", read_temperature_handler);
 
- 
+#endif
+
 LOG_MODULE_REGISTER(I2C_SHELL);
 
 int main(void)
 {	
 
-	if (!device_is_ready(i2c5_bus))
-    {
-        LOG_ERR("I2C5 device not found!");
-        return 1;
-    }
+	#if CONFIG_BOARD_FRDM_MCXN947 == 1
+
+		if (!device_is_ready(i2c5_bus))
+		{
+			LOG_ERR("I2C5 device not found!");
+			return 1;
+		}
+	#endif
 
 
 	while(1)
