263079cba246964b2529cbe1033b7f520fc1dab6
Eli Hughes <eli.hughes@nxp.com>
2024-10-21 19:48:29 -0400

    bb project works

--- demos/bunny_build/src/ezh_app.c
+++ demos/bunny_build/src/ezh_app.c
@@ -2,6 +2,7 @@
 #include <zephyr/irq.h>
 
 #include "fsl_common.h"
+#include "fsl_gpio.h"
 #include "fsl_clock.h"
 #include "ezh_init.h"
 #include <ezh_app.h>
@@ -21,11 +22,10 @@ uint32_t test_val = 0;
 #define   EZH__Reserved46_IRQn             (IRQn_Type) 30
 
 //LPC55 has quick/direct access to GPIO Port 0
-//Using PIO_3 for test GPIO.  Maps to GPIO1 silkscreen label on gibbon D
-
+//Using PIO_3 for test GPIO. 
 #define   EZH_TEST_GPIO                         3        
 
-void ezh_app__toggle_gibbon_test_io(void)
+void ezh_app__toggle_test_io(void)
 {
     E_NOP;
 
@@ -66,125 +66,13 @@ void ezh_app__toggle_gibbon_test_io(void)
     E_PER_WRITE(R0, EZH2ARM);
 
     E_STR(R7,R0,0); //store the counter in the 1st parameter
-    E_STR(R7,R1,1); //store the  double speed counter in the 2nd parameter
+    E_STR(R7,R1,1); //store the double speed counter in the 2nd parameter
 
 
     E_WAIT_FOR_BEAT();
     E_WAIT_FOR_BEAT();
 
     E_GOSUB("toggle");
-}
-
-
-
-/*
-
-    EZH SPI MEM parameter memory layout 
-
-    R6 will be inialized from the 
-
-    R6 + 0  --> initial stack pointer
-    R6 + 1  --> command
-    R6 + 2  --> data buffer pointer
-    R6 + 3  --> data buffer length
-    R6 + 4  --> target address in spi ram
-
-    Commands:
-
-    0x00  -- Idle
-    0x01  -- toggle test IO
-
-    0x02  -- Write
-    0x03  -- Read
-
-*/
-
-
-#define TEMP_REG1                R0
-#define TEMP_REG2                R1
-
-#define PARAM_STRUCT_PTR_REG    R6
-#define PARAM_OFFSET__STACK_PTR  0
-#define PARAM_OFFSET__CMD        1
-
-#define COMMAND_REG             R3
-
-#define CMD__TOGGLE_TEST_IO     1
-
-
-void ezh__send_cmd(uint32_t cmd)
-{
-    ezh_debug_params[PARAM_OFFSET__CMD] = cmd;
-}
-
-void ezh_app__spi_mem (void)
-{
-    E_NOP;
-    E_NOP;
-    E_PER_READ(TEMP_REG1, ARM2EZH); //Read the base address of the parameters into R6
-
-    E_LSR(TEMP_REG1, TEMP_REG1, 2);        //make sure parameter structure 32-bit aligned
-    E_LSL(TEMP_REG1, TEMP_REG1, 2);        
-
-    E_LDR(SP, TEMP_REG1, 0);        //load stack pointer from the 1st 32-bit word of the parameter struct
-    E_LDR(PARAM_STRUCT_PTR_REG, TEMP_REG1, 1);        //load parameter reg pointer
-
-
-
-   /* Set EZH_TEST_GPIO  as output , Direction bit = 1 */
-    E_BSET_IMM(GPD, GPD, EZH_TEST_GPIO);
-
-    //Make the default setting as high
-    E_BCLR_IMM(GPO, GPO, EZH_TEST_GPIO);
-
-    /* set the 16-bit heartbeat counter period to 250uS assuming 150MHz Clock*/
-    E_HEART_RYTHM_IMM((150000000 / 4000));
-
- E_LABEL("reset_cmd");
-    E_SUB(COMMAND_REG,COMMAND_REG,COMMAND_REG);  //zero out the command register
-    E_STR(PARAM_STRUCT_PTR_REG,COMMAND_REG,PARAM_OFFSET__CMD); //put the zero'd out regsiter back int the structure for the arm
-
- E_LABEL("wait_for_command");
-
-
-    E_LDR(COMMAND_REG, PARAM_STRUCT_PTR_REG,PARAM_OFFSET__CMD);        //Check to see if we have  new command
-    E_ADD(COMMAND_REG,COMMAND_REG,0);
-
-    E_COND_GOTO(ZE,"no_cmd");
-    E_NOP;
-    E_NOP;
-
- //E_BCLR_IMM(GPO, GPO, EZH_TEST_GPIO);
-  //E_BSET_IMM(GPO, GPO, EZH_TEST_GPIO);
-
-    E_SUB_IMM(TEMP_REG2, COMMAND_REG, CMD__TOGGLE_TEST_IO); 
-    E_COND_GOTO(ZE, "toggle_test_io");
-
-    E_GOSUB("reset_cmd");
-
-E_LABEL("toggle_test_io");
- 	E_BSET_IMM(GPO, GPO, EZH_TEST_GPIO); //Set test GPIO
-	E_WAIT_FOR_BEAT();
-	E_WAIT_FOR_BEAT();
-    E_BCLR_IMM(GPO, GPO, EZH_TEST_GPIO); //Clear test GPIO
-    E_WAIT_FOR_BEAT();
-    E_WAIT_FOR_BEAT();
-    E_BSET_IMM(GPO, GPO, EZH_TEST_GPIO); //Set test GPIO
-	E_WAIT_FOR_BEAT();
-	E_WAIT_FOR_BEAT();
-    E_WAIT_FOR_BEAT();
-    E_BCLR_IMM(GPO, GPO, EZH_TEST_GPIO); //Clear test GPIO
-    E_WAIT_FOR_BEAT();
-    E_WAIT_FOR_BEAT();
-    E_WAIT_FOR_BEAT();
-
-    E_PER_WRITE(COMMAND_REG, EZH2ARM);  //notify the arm that the command is complete
-    E_GOSUB("reset_cmd");
-
-
-
- E_LABEL("no_cmd");
-    E_GOSUB("wait_for_command");
 }
 
 ISR_DIRECT_DECLARE(Reserved46_IRQHandler__EZH)
@@ -210,7 +98,7 @@ void ezh__start_app()
 
     
     //Magic mux setting to use port 0.3 directly in EZH
-    IOCON->PIO[0][3]             = PINFUNC_EZH | 2<<4 | 1<<8;
+    IOCON->PIO[0][3]  = PINFUNC_EZH | 2<<4 | 1<<8;
 
 
 	CLOCK_EnableClock(kCLOCK_Ezhb);        // enable EZH clock
@@ -222,7 +110,7 @@ void ezh__start_app()
 
 	bunny_build(&my_ezh_program[0],
 	    	    sizeof(my_ezh_program),
-				ezh_app__spi_mem
+				ezh_app__toggle_test_io
 				);
 
 	EZH_boot(my_ezh_program);				     //start EZH
