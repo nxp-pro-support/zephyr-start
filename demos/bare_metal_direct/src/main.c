f08c1822ae15c5da88d67d7906e9d7f8aad9d1b1
Eli Hughes <eli.hughes@nxp.com>
2024-10-24 08:05:21 -0400

    minor bare metal updates

--- demos/bare_metal_direct/src/main.c
+++ demos/bare_metal_direct/src/main.c
@@ -2,24 +2,36 @@
 #include <zephyr/logging/log.h>
 #include <zephyr/shell/shell.h>
 
+/*
+    From the MCUXpresso SDK
+*/
 #include "fsl_clock.h"
 #include "fsl_device_registers.h"
 
-#define LED_PIN     10
+
+//Red LED on FRDM-MCX is on GPIO0  Port 10
+#define RED_LED_PIN           10
+#define RED_LED_GPIO          GPIO0
 
 LOG_MODULE_REGISTER(main);
 
 int main(void)
 {
-    LOG_INF("Bare-metal-direct application");
+    LOG_INF("Bare-metal-direct application for MCXN947");
 
+    //Enable the clock on GPIO0
     CLOCK_EnableClock(kCLOCK_Gpio0);
 
-    GPIO0->PDDR |= (1 << LED_PIN);
+    //Set to output mode
+    GPIO0->PDDR |= (1 << RED_LED_PIN);
+
+    //Clear the pin
+    GPIO0->PCOR = RED_LED_PIN;
 
     while (1)
     {   
-        GPIO0->PTOR |= (1 << LED_PIN);
+        GPIO0->PTOR |= (1 << RED_LED_PIN);
+       
         k_sleep(K_MSEC(500));
     }
     
